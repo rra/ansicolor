f2482cd3f3aaef67aab89f3b42529e5e9950967e
Russ Allbery <rra@cpan.org>
2026-07-31 19:39:30 -0700

    Show that colored takes an array in SYNOPSIS
    Provide an example that shows that the `colored` function can
    take an array of multiple attributes as well as a space-separated
    string. Thanks, Elvin Aslanov.

--- lib/Term/ANSIColor.pm
+++ lib/Term/ANSIColor.pm
@@ -650,7 +650,7 @@ strikethrough truecolor Nicol
     print "This text is normal.\n";
     print colored(['yellow on_magenta'], 'Yellow on magenta.', "\n");
     print colored(['red on_bright_yellow'], 'Red on bright yellow.', "\n");
-    print colored(['bright_red on_black'], 'Bright red on black.', "\n");
+    print colored(['bright_red', 'on_black'], 'Bright red on black.', "\n");
     print "\n";
 
     # Map escape sequences back to color names.
